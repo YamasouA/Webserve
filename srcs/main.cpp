b0ed665218adea6a25b8db930384a3cd0650f27d
YamasouA <akiakiskyhand@gmail.com>
2023-05-02 00:03:42 +0900

    [] IO多重化(仮)

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,6 +1,9 @@
 #include "Socket.hpp"
 #include <iostream>
-
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/event.h>
+#include <sys/ioctl.h>
 
 int main(int argc, char *argv[]) {
 	// 設定ファイルを読み込む
@@ -8,15 +11,16 @@ int main(int argc, char *argv[]) {
 	(void)argc;
 	(void)argv;
 	std::string default_file = "index.html";
-	Socket *socket = new Socket(5000); //port num is tmp
+	Socket *socket = new Socket(8000); //port num is tmp
 	socket->set_socket();
+
 	int acceptfd;
-	ssize_t ret;
-	char buf[1024];
+	//ssize_t ret;
+	//char buf[1024];
 
 	struct kevent register_event[4], reciver_event[4];
 	int kq = kqueue();
-	EV_SET(register_event, socket->get_listenfd(), EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, NULL);
+	EV_SET(register_event, socket->get_listenfd(), EVFILT_READ, EV_ADD | EV_ENABLE , 0, 0, NULL);
 	if (kevent(kq, register_event, 1, NULL, 0, NULL) == -1) {
 		perror("kevent");
 		std::exit(1);
@@ -28,11 +32,12 @@ int main(int argc, char *argv[]) {
 			perror("kevent");
 			std::exit(1);
 		}
-		for (size_t i = 0; i < events_num; ++i) {
+
+		for (int i = 0; i < events_num; ++i) {
 			int event_fd = reciver_event[i].ident;
-			if (event[i].flags & EV_EOF) {
+			if (reciver_event[i].flags & EV_EOF) {
 				std::cout << "Client has disconnected" << std::endl;
-				std::close(event_fd);
+				close(event_fd);
 			}
 			else if (event_fd == socket->get_listenfd()) {
 				acceptfd = accept(socket->get_listenfd(), NULL, NULL);
@@ -40,15 +45,16 @@ int main(int argc, char *argv[]) {
 					std::cerr << "Accept socket error" << std::endl;
 					continue;
 				}
-				EV_SET(register_event, acceptfd, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, NULL);
+				EV_SET(register_event, acceptfd, EVFILT_READ, EV_ADD, 0, 0, NULL);
 				if (kevent(kq, register_event, 1, NULL, 0, NULL) == -1) {
 					perror("kevent error");
 				}
+			} else if (reciver_event[i].filter & EVFILT_READ) {
+				char buf[1024];
+				memset(buf, 0, sizeof(buf));
+				recv(event_fd, buf, sizeof(buf), 0);
+				std::cout << buf << std::endl;
 			}
 		}
-		memset(buf, 0, sizeof(buf));
-		ret = read(acceptfd, buf, 1024);
-		std::cout << ret << std::endl;
-		std::cout << buf << std::endl;
 	}
 }
