dae20393d364575c4b81e43a51472d12afb8546d
YamasouA <akiakiskyhand@gmail.com>
2023-05-15 18:56:20 +0900

    [WIP]

---
+++ srcs/http/HttpRes.hpp
@@ -0,0 +1,28 @@
+#ifndef HTTPRES_HPP
+#define HTTPRES_HPP
+
+#include <string>
+#include <sstream>
+#include <vector>
+#include "../conf/Location.hpp"
+#include "../conf/virtualServer.hpp"
+#include "httpReq.hpp"
+#include "../Client.hpp"
+class HttpRes {
+	private:
+		std::string body;
+		std::string head;
+		// request, vserverはclientのをそのまま使うからデータの持ち方どうしよう
+		// 親のクライアントへの参照を持つのはあり
+		httpReq httpReq;
+		virtualServer vServer;
+		Location target;
+	public:
+		HttpRes(const Client& source);
+		~HttpRes();
+		void longestMatchLocation();
+		void read_file();
+		void createResponse();
+};
+
+#endif
