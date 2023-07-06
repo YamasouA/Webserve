83163423768f026297f6b2d51700076ddad94a0d
lim <lbooks424@gmail.com>
2023-07-06 23:06:51 +0900

    [WIP]

--- srcs/http/cgi.cpp
+++ srcs/http/cgi.cpp
@@ -19,7 +19,7 @@ bool Cgi::is_meta_var() {
 	std::map<std::string, std::string> envs;
 
 	for (; it != httpreq.end(); it++) {
-		if 
+		if
 	}
 }
 
@@ -28,21 +28,46 @@ bool Cgi::check_meta_var(string var) {
 	// フォーマットがあるやつはそれを確認
 
 	if (var == "auth_type" || var ==  "content_length" || var ==  "content_type"
-		var == "gateway_interface" || var ==  "path_info" || var ==  "path_translated" || 
+		var == "gateway_interface" || var ==  "path_info" || var ==  "path_translated" ||
 		var == "query_string" ||  var == "remote_addr" ||  var == "remote_host" || var == "remote_ident", "remote_user", "request_method", "script_name",
 							"server_name", "server_port", "server_protocol", "server_software"}
 }
 
+
+
+// "AUTH_TYPE" | "CONTENT_LENGTH" |
+// "CONTENT_TYPE" | "GATEWAY_INTERFACE" |
+// "PATH_INFO" | "PATH_TRANSLATED" |
+// "QUERY_STRING" | "REMOTE_ADDR" |
+// "REMOTE_HOST" | "REMOTE_IDENT" |
+// "REMOTE_USER" | "REQUEST_METHOD" |
+// "SCRIPT_NAME" | "SERVER_NAME" |
+// "SERVER_PORT" | "SERVER_PROTOCOL" |
+// "SERVER_SOFTWARE" | scheme |
+//  protocol-var-name | extension-var-name"
+
+
 void Cgi::set_env() {
+    // httpreq will have meta var info with map<std::string, std::string>
+    //
 	std::map<std::string, std::string>::iterator it = httpreq.begin();
 	char **env_ptr;
 	std::map<std::string, std::string> envs;
 
 	for (; it != httpreq.end(); it++) {
-		envs[it->first] = it->second;
+	    if (check_meta_var() {
+		    envs[it->first] = it->second;
+        }
 	}
-
-	check_meta_var();
+    //CONTENT_LENGTH is set exist message-body [MUST]
+    //CONTENT_TYPE is set exist message-body　セットされていない場合はスクリプトが受信したデータのmime型を決定しようと試みる可能性がある
+    //  未知のままであれば、スクリプトは型を application/octet-stream とみなすかもしれないし、誤りとして拒絶するかもしれない
+    //  リクエストにCONTENT_TYPEが存在した場合はsetしなければならない [MUST]
+    //GATEWAY_INTERFACE is must set value [MUST] CGI/1.1
+    //PATH_INFO　文字大小保存 制限を課しても課さなくても良い
+    //
+    //
+    // set sever info meta vars
 }
 
 void Cgi::send_body() {
